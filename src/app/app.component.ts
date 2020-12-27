7bf63cdea5b30017455fd7198b018b3c785d3515
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-27 11:55:52 +0530

    ADDED:Property binding for img tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World';
+  imgUrl="";
 
   ngOnInit():void{
     this.title="Hello From BridgeLabz"
+    this.imgUrl="../assets/BL_logo_square_jpg.jpg"
   }
 }
